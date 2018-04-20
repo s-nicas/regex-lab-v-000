b1bbb210fee3e43c52d0dfa5468b24451bbde827
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-20 21:18:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,34 @@
 def starts_with_a_vowel?(word)
 
+  if /\A[aeiou]|[AEIOU]/.match(word) 
+    return true 
+  else 
+    return false 
+ end
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
+  text.scan(/\b+un\w+ing/) 
+
+
 end
 
 def words_five_letters_long(text)
+  words = text.split
+  
+  words.grep(/^\w{5}$/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  
+  if text.scan(/^[A-Z]{1}/).length == 1 && text.scan(/\W$/).length == 1
+    return true 
+  else 
+    return false 
+  end 
 
 end
 
